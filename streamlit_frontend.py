96b7cd183dbe3f9f1d2fd1282e53f2daa871d804
Vaibhav Goel <vaibhavgoel@Mac.lan>
2026-01-25 16:38:10 +0530

    Added Streaming Based Response Generation

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,3 +1,4 @@
+from importlib.metadata import metadata
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
@@ -20,9 +21,11 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
 
-    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
-
-    assistant_message = response["messages"][-1].content
-    st.session_state.message_history.append({"role": "assistant", "content": assistant_message})
     with st.chat_message("assistant"):
-        st.text(assistant_message)
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]}, config=config, stream_mode="messages"
+            )
+        )
+
+    st.session_state.message_history.append({"role": "assistant", "content": ai_message})
